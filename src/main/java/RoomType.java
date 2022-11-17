95a6ac95957eab3ce3e035979714f9d62f1d50a3
Colin Cameron <ccameron2020@gmail.com>
2022-11-17 11:18:38 +0000

    RoomType done

--- src/main/java/RoomType.java
+++ src/main/java/RoomType.java
@@ -1,2 +1,23 @@
 public enum RoomType {
+    SINGLE(1),
+    DOUBLE(2),
+    TRIPLE(3),
+    FAMILY(4);
+
+    private final int value;
+    private final int capacity;
+
+    RoomType(int value, int capacity){
+        this.value = value;
+        this.capacity = capacity;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    Guest guest1;
+
+    @Before
+    public void before(){
+        guest1 = new Guest("Randolf");
+    }
+    @Test
+    public void hasName(){
+        assertEquals("Randolf", guest1.getName());
+    }
+
 }
